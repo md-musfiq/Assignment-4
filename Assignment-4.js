f8eb9a62abe31b83500312bf32f938599b162b3d
md-musfiq <md7mushfqur2rahman@gmail.com>
2022-08-10 22:38:14 +0600

    second time code push

---
+++ Assignment-4.js
@@ -0,0 +1,11 @@
+// Problem:1  radianToDegree
+
+function radianToDegree(radians){
+  var pi = Math.PI;
+  const radian = (radians * (180/pi)).toFixed(2);
+  return radian;
+}
+
+
+// Problem:2  isJavaScriptFile
+
